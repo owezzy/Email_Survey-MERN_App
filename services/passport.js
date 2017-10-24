fa5f45e3790a6663a22850e13a3df6f228f05c8d
Owen Adira <owenobezzy@gmail.com>
2017-10-24 18:41:25 +0300

    Route set up Header navbar layout complete

--- services/passport.js
+++ services/passport.js
@@ -1,46 +1,41 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose =  require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-    "use strict";
-    done(null, user.id);
+  "use strict";
+  done(null, user.id);
 });
 
-passport.deserializeUser((id, done) =>{
-    "use strict";
-    User.findById(id)
-        .then(user  => {
-            done(null, user);
-        });
+passport.deserializeUser((id, done) => {
+  "use strict";
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
-
-
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: "/auth/google/callback",
-            proxy: true
-        },
-        (accessToken, refreshToken, profile, done) => {
-           User.findOne({googleId: profile.id })
-               .then((existingUser)=> {
-               "use strict";
-               if (existingUser){
-                   //profile id is present in DB
-                   done(null,existingUser);
-               }else{
-                   // save the new user profile id to db
-                   new User({googleId: profile.id}).save().then(user => done(null, user));
-               }
-               });
-
-        }
-    )
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      ("use strict");
+      if (existingUser) {
+        //profile id is present in DB
+        done(null, existingUser);
+      } else {
+        // save the new user profile id to db
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
+    }
+  )
 );
